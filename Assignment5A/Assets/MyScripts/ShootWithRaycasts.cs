541d7d7955238e1fd3017e620456c73eeeaf11e9
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-10-28 15:33:05 -0500

    work on demo

--- Assignment5A/Assets/MyScripts/ShootWithRaycasts.cs
+++ Assignment5A/Assets/MyScripts/ShootWithRaycasts.cs
@@ -6,6 +6,10 @@ public class ShootWithRaycasts : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float hitForce = 10f;
+
+    public ParticleSystem muzzleFlash;
+
     public Camera cam;
     // Start is called before the first frame update
     void Start()
@@ -21,10 +25,23 @@ public class ShootWithRaycasts : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hitInfo;
         if(Physics.Raycast(cam.transform.position,cam.transform.forward, out hitInfo, range))
         {
             Debug.Log(hitInfo.transform.gameObject.name);
+
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            if (hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
         }
     }
 }
